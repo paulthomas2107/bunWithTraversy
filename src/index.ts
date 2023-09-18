bb58e7b398a5022d9679530aa993f0b787590bda
Paul Thomas <paulthomas2107@gmail.com>
2023-09-18 15:41:06 +0100

    More

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import fetchUser from './githubAPI';
-
-(async () => {
-  const userData = await fetchUser('paulthomas2107');
-  document.querySelector('h1').innerHTML = JSON.stringify(userData);
-})();
